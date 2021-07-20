82faad036f069645e50603dfaaf7767704756cf0
caorui <1412185242@qq.com>
2021-07-20 15:26:21 +0800

    none

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'fa12bda25bcd3ccfde0fd4ad8bd01e56837c4ed4',
+        'name' => 'aex/for-laravel-test',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'aex/for-laravel-test' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'fa12bda25bcd3ccfde0fd4ad8bd01e56837c4ed4',
+            'dev_requirement' => false,
+        ),
+    ),
+);
